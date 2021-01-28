afa730b7165f200c2392f1bfd3e2de2789120f89
Luis <luis.here.j@gmail.com>
2021-01-28 14:28:10 +0100

    Añadido schema a vehiculo.js

---
+++ models/vehiculo.js
@@ -0,0 +1,56 @@
+// const { Schema, model } = require ('mongoose');
+
+const rentcarSchema = new Schema({
+    size: {
+        type: Array,
+        required: true
+    },
+
+    carBrand: {
+        type: String,
+        required: true
+    },
+
+    carModel: {
+        type: String,
+        required: true
+    },
+
+    motorType: {
+        type: Array,
+        required: true
+    },
+
+    transmissionType: {
+        type: Array,
+        required: true
+    },
+
+    numberOfSeats: {
+        type: Array,
+        required: true
+    },
+
+    trunkSize: {
+        type: Number,
+        required: true
+    },
+
+    airConditioning: {
+        type: Boolean,
+        required: true
+    },
+
+    co2Emissions: {
+        type: Number,
+        required: true
+    },
+
+    carImage: {
+        type: String,
+        required: true
+    },
+});
+
+
+// module.exports = model('RentCar', rentcarSchema, 'rentcars');
