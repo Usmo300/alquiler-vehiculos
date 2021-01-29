1da1a9d8ac975dbee9aa16a226dbf8bb11f7c638
Luis <luis.here.j@gmail.com>
2021-01-29 10:00:17 +0100

    Cambio orden Schema

--- models/RentCar.js
+++ models/RentCar.js
@@ -1,6 +1,11 @@
 const { Schema, model } = require ('mongoose');
 
 const rentcarSchema = new Schema({
+    transactionType:{
+        type: String,
+        required: true
+    },
+    
     size: {
         type: String,
         required: true
