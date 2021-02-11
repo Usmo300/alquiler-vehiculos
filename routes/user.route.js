b2a3480bff81f6df178fdd49fe31d53c741c47f5
Luis <luis.here.j@gmail.com>
2021-02-11 09:51:59 +0100

    renombre ruta user

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@
 
 const {Router} = require('express');
 const router = Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/user.controller');
 
 router.get("/users/add", userController.formCreateUser);
 
