a207104caa0170f4cd6404a3bfc5efb83565a645
Arturo <arturomf88@gmail.com>
2021-02-12 10:07:21 +0100

    cambios para el testing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
-app.use(require('./routes/Car.route'));
+app.use(require('./routes/Car.routes'));
 
 app.set('view engine', 'hbs');
 
