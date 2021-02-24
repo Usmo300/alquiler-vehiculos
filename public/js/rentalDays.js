599c143a748a235281894c5b79985111def52241
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-02-24 09:35:37 +0100

    Archivo rentalDays.js
    funcion rentalDays() y función finalPrice() para calcular el precio del alquiler

--- public/js/rentalDays.js
+++ public/js/rentalDays.js
@@ -1,13 +1,26 @@
 'use strict';
 
-
+// let fecha = document.querySelector('input[type="date"]');
+// console.log(fecha.value);
 
 function caculateDays(){
 	let beginDate = new Date(document.querySelector('#leaseDate').value);
     console.log(beginDate);
 	let endDate = new Date(document.querySelector('#leaseReturnDate').value);
-	let rentalDays= endDate.getTime()-beginDate.getTime();
+    console.log(endDate);
+	let rentalDays= endDate.getTime() - beginDate.getTime();
 	let numberOfDays = Math.round(rentalDays/(1000*60*60*24));
 	console.log(numberOfDays);
 };
-caculateDays();
+const button = document.querySelector('#confirmDates');
+button.addEventListener('click', caculateDays);
+
+const pricePerDay= document.querySelector('#leasingPrice').value;
+console.log(pricePerDay);
+
+function calculatePrice(numberOfDays, pricePerDay){
+    const finalPrice = numberOfDays * pricePerDay;
+    console.log(finalPrice);
+}
+button.addEventListener('click', calculatePrice);
+
