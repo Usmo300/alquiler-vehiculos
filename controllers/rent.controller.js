f412dbbfcd9c97fca768130d14d3293a78e4aff1
Abraham <nencho_1@hotmail.com>
2021-03-08 14:53:08 +0100

    fixed problem on the hbs and created gitignore

--- controllers/rent.controller.js
+++ controllers/rent.controller.js
@@ -11,22 +11,20 @@ RentController.leaseCar = async (req, res) => {
     const Ndays = (new Date((req.body.leaseReturnDate)).getTime() - new Date((req.body.leaseDate)).getTime())/(24*60*60*1000);
     console.log(Ndays);
     const finalPrice = (singleCar.leasingPrice*Ndays);
-    console.log(finalPrice);
-    // const leaseReturnDate = (new Date((req.body.leaseDate)).getTime() + ((req.body.Ndays - 1)*24*60*60*1000));
-    const newRent = new Rent ({carBrand: singleCar.carBrand,
+    const newRent = new Rent ({
+        carBrand: singleCar.carBrand,
         carModel: singleCar.carModel,
         leasingPrice: finalPrice,
         leaseDate: req.body.leaseDate,
         leaseReturnDate: req.body.leaseReturnDate,
-        carId: singleCar._id});
-        console.log(newRent);
-        // if (singleCar.availability === "yes") {
-       const car = await newRent.save();
-       console.log(car);
-        // await carUpdate(req.params.id, {availability: "no"});
- 
-    res.render("rent/rentDetailTemplate", {car});
-    // res.redirect('/rentalcars/transactions');
+        carId: singleCar._id
+    });
+
+    const car = await newRent.save();
+    const convertArray = [];
+    convertArray.push(car)
+
+    res.render("rent/rentDetailTemplate", { convertArray });
 };
 
 RentController.showAllRents = async (req,res) => {
@@ -34,12 +32,5 @@ RentController.showAllRents = async (req,res) => {
     rentUpdate(rentsList);
     res.render("rent/allRents", {rents: rentsList} );
 };
-
-// RentController.showOneRent = async (req, res) => {
-//     const rentDetail = await getSingleRent(req.params.id);
-//     res.render('rent/rentDetailTemplate', {rentDetail});
-// };
-
-
 
 module.exports = RentController;
